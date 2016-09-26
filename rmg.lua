1a002ab3fca933cee103b4ae38c79eb1b9378700
Dens0n <dennis.magnusson98@gmail.com>
2016-09-26 14:24:01 +0200

    Made som changes

--- rmg.lua
+++ rmg.lua
@@ -46,9 +46,9 @@ end
 function fit(model, criterion, lr, batch)
 	--for i = 1, #batch[1] do
 	for i = 1, 330 do
-		--print(i)
-		local x = batch[1][i]
-		local y = batch[2][i]
+		print(i)
+		local y = batch[2][i]--And here
+		local x = batch[1][i]--Breaks here
 		local pred = model:forward(x)
 		--local err  = criterion:forward(pred, y)
 		local gradcrit = criterion:backward(pred, y) 
@@ -68,24 +68,33 @@ function train(model, data, ep)
 	
 	local maxlen = get_maxlen(data)
 	for i = 2, maxlen do
-		local x = {}
-		local y = {}
+		--local x = {}
+		--local y = {}
+		local x = torch.Tensor(#data, maxlen, 88)
+		local y = torch.Tensor(#data, 88)
 		for key, d in pairs(data) do
 			local p = {}
 			for k = 1, i-1 do
-				p[#p+1] = d[k]
+				for u = 1, 88 do
+					x[key][i][u] = d[k][u]
+				end
 			end
-			x[#x+1] = p
-			y[#y+1] = d[i]
+			--x[#x+1] = p
+			--y[#y+1] = d[i]
+			for u = 1, 88 do
+				y[key] = d[i][u]
+			end
 		end
-		print(#x[1], #y[1])
 		--x = torch.expand(torch.Tensor(x), maxlen, 88)
-		x = torch.zeros(maxlen, 88)--Something with this --TODO Convert to tensor filled with zeros of same size to solve problem
+		--x = fill(x, maxlen, 88)
+		--x = torch.zeros(maxlen, 88)--TODO Convert to tensor filled with zeros of same size to solve problem
+		x = torch.Tensor(x)
 		y = torch.Tensor(y)
+		print(x:size(), y:size())
 		--local batch = {torch.Tensor(x), torch.Tensor(y)}
 		local batch = {x, y}
-		--trainer.train(batch)
-		fit(model, criterion, 0.01, batch)
+		trainer.train(batch)
+		--fit(model, criterion, 0.01, batch)
 	end
 	
 	--[[
@@ -98,6 +107,24 @@ function train(model, data, ep)
 	end
 	]]
 	model.evaluate() --Exit training mode
+end
+
+function fill(r, rows, cols)
+	local empty = {}
+	print(r, #r)
+	for k=1, cols do
+		empty[k] = 0
+	end
+	print(#empty)
+
+	for i = 1, rows do
+		if not r[i] then
+			r[i] = empty
+		end
+		--if #r[i] ~= 88 then print(#r[i], i) end
+	end
+	--local x = torch.Tensor(r)
+	return r
 end
 
 function get_maxlen(data)
@@ -139,11 +166,14 @@ function create_model()
 	local hiddensize = 256
 	local dropoutprob = 0.2
 	local model = nn.Sequential()
+	local layer1 = nn.SeqLSTM(88,88, 100)
+	layer1:maskZero()
+	model:add(layer1)
 	--rho(Steps to backpropagate) = 100
 	--layer1=nn.FastLSTM(88, hiddensize, rho)
 	--layer1:maskZero(1)
 	--model:add(layer1)
-	model:add(nn.FastLSTM(88, hiddensize, rho))--Hope this works
+	--model:add(nn.FastLSTM(88, hiddensize, rho)):MaskZero()--Hope this works
 	model:add(nn.Tanh())
 	--model:add(nn.FastLSTM(hiddensize, hiddensize, rho))
 	--model:add(nn.Tanh())
