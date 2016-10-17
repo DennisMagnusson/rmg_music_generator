e21eb87878469f778c3ce2a391ddeb2f2aa1ee6b
d <dennis.magnusson98@gmail.com>
2016-10-17 17:11:46 +0200

    Add time and velocity

--- midigen.lua
+++ midigen.lua
@@ -2,25 +2,36 @@ local midi = require 'MIDI'
 
 function generate(pattern, ...)
 	local arg = table.pack(...)
-	local score = {1, {}}
+	local score = {1000, {}}
 	local time = 0
+	pattern = to_abs_time(pattern)
 	for k, frame in pairs(pattern) do
 		local tones = {}
-		for n, i in pairs(frame) do
-			if i ~= 0 then tones[#tones+1] = n end
+		for n=1, 88 do
+			if frame[n] ~= 0 then tones[#tones+1] = n end
 		end
 
 		for i, tone in pairs(tones) do
-			score[2][#score[2]+1] = {'note', time, 1, 1, tone+20, 127}
+			score[2][#score[2]+1] = {'note', frame[90], frame[91], 1, tone+20, math.floor(frame[89]*127)}
 		end
 		time = time+1
 
 	end
-	print(arg[1])
+	table.insert(score[2], {'set_tempo', 0, 1000000})
+
 	if arg[1] then
 		local file = assert(io.open("generated/" .. arg[1], 'w'))
 		file:write(midi.score2midi(score))
 		file:close()
 	end
 
+end
+
+function to_abs_time(r)
+	local t = 0
+	for k, frame in pairs(r) do
+		t = t + frame[90]
+		frame[90] = t
+	end
+	return r
 end
--- midiparse.lua
+++ midiparse.lua
@@ -2,7 +2,7 @@ local midi = require 'MIDI'
 
 function parse(filename)
 	local file = assert(io.open(filename, 'r'))
-	local m = midi.midi2score(file:read("*all"))
+	local m = midi.midi2ms_score(file:read("*all"))
 	file:close()
 	file = nil
 	local r = {}
@@ -12,11 +12,17 @@ function parse(filename)
 		if event[1] == 'note' then notes[#notes+1] = event end
 	end
 
+	notes = sort_by_time(notes)
+
 	local i = 0
 	for k, event in pairs(notes) do
-		--If the times are the same
-		if (i > 1 and event[2] == notes[i-1][2]) then
-			r[#r-1][event[5]-20] = 1
+		if #r > 1 and event[2]-notes[k-1][2] == 0 then
+			--tone
+			r[#r][event[5]-20] = 1
+			--velocity
+			if event[6]/127 > r[#r][89] then r[#r][89] = event[6]/127 end
+			--duration
+			if event[3] > r[#r][91] then r[#r][91] = event[3] end
 			i = i+1
 			goto EOL
 		end
@@ -24,20 +30,39 @@ function parse(filename)
 		--Fill frame with zeros
 		local frame = {}
 		for i = 1, 88, 1 do frame[i] = 0 end
+		--tone
 		frame[event[5]-20] = 1
+		--velocity
+		frame[89] = event[6]/127
+		--delta start_time
+		if #r <= 1 then 
+			frame[90] = 0
+		else
+			frame[90] = event[2] - notes[k-1][2]
+		end
+		--duration
+		frame[91] = event[3]
+		--Insert into r
 		r[#r+1] = frame
 		i = i+1
 		::EOL::
 	end
-	
 	return r
 end
 
-function print_index(r)
-	for key, frame in pairs(r) do
-		for n, i in pairs(frame) do
-			if i ~= 0 then io.write(n.." ") end
+function sort_by_time(r)
+	local s = {}
+	s[1] = r[1]
+	for i=2, #r do
+		local l = #s
+		for u=1, #s do
+			if r[i][2] < s[u][2] then 
+				table.insert(s, u, r[i])
+				goto cont
+			end
 		end
-		print("")
+		if s[l+1] == nil then s[l+1] = r[i] end
+		::cont::
 	end
+	return s
 end
