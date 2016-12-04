21415002cffeb8e5f03d90eff0e5ac1d44a8f94c
d <dennis.magnusson98@gmail.com>
2016-12-04 16:25:02 +0100

    Fixed validation

--- validate.lua
+++ validate.lua
@@ -12,79 +12,60 @@ local minor = {0, 2, 3, 5, 7, 8, 10}
 local valid_data = {}
 
 function validate(model, rho, dir, criterion)
-	if not valid_data[1] then valid_data = create_data(rho, dir) end
+	if not valid_data[1] then valid_data = create_data(dir) end
+
 	local toterr = 0
-	for i=1, #valid_data[1] do
-		local x = valid_data[1][i]:cl()
-		local y = valid_data[2][i]:cl()
+	local c = 0
+	local bs = 0
 
-		local pred = model:forward(x)
-		local err = criterion:forward(pred, y)
-		toterr = toterr + err
-	end
+	local batchsize = 350
 
-	return toterr / #valid_data[1]
-end
+	local x = torch.Tensor(batchsize, rho, 93)
+	local y = torch.Tensor(batchsize, 93)
 
-function create_frame(index)
-	local frame = {}
-	for i=1, 93 do
-		frame[i] = 0
-	end
-
-	frame[index] = 1
-	frame[91] = 0.7
-	frame[92] = math.log(201)/8.29
-	frame[93] = math.log(150)/8.29
-
-	return frame
-end
-
-function create_data(rho, dir)
-	local x = {}
-	local y = {}
-	--Scales
-	for i=40, 52 do
-		for k=1, 8 do
-			--Major scale
-			x[#x+1] = torch.zeros(rho, 93):cl()
-			for f=1, #major-1 do
-				local frame = create_frame(i+major[f])
-				for q=1, 93 do
-					x[#x][rho-f][q] = frame[q]
-				end
+	for _, song in pairs(valid_data) do
+		for i=1, #song-rho-1 do
+			for k=rho+i-1, i, -1 do
+				x[bs+1][k-i+1] = torch.Tensor(song[k])
 			end
-			y[#y+1] = torch.Tensor(create_frame(i+major[#major]))
+			y[bs+1] = torch.Tensor(song[rho+i+1])
+			bs = bs+1
 
-			--Minor scale
-			x[#x+1] = torch.zeros(rho, 93):cl()
-			for f=1, #minor-1 do
-				local frame = create_frame(i+minor[f])
-				for q=1, 93 do
-					x[#x][rho-f][q] = frame[q]
-				end
+			if bs == batchsize then
+				local pred = model:forward(x:cl())
+				local err = criterion:forward(pred, y:cl())
+				toterr = toterr + err
+				x = torch.Tensor(batchsize, rho, 93)
+				y = torch.Tensor(batchsize, 93)
+				c = c+1
+				bs = 0
 			end
-			y[#y+1] = torch.Tensor(create_frame(i+minor[#minor]))
+			
 		end
 	end
+	
+	return toterr / c
+end
 
-	--Actual songs
+function normalize(r, col)
+	for i=1, #r do
+		for u=1, #r[i] do
+			if r[i][u][col] > 4000 then r[i][u][col] = 4000 end
+			r[i][u][col] = math.log(r[i][u][col]+1) / 8.29
+		end
+	end
+	return r
+end
+
+function create_data(dir)
 	local songs = {}
 	for filename in lfs.dir(dir.."/.") do
 		if filename[1] == '.' then goto cont end
 		local song = parse(dir.."/"..filename) 
+		songs[#songs+1] = song
 		::cont::
 	end
-	--Should work
-	for _, songs in pairs(songs) do
-		for i=1, #song-rho-1 do
-			x[#x+1] = torch.zeros(rho, 93)
-			for k=i, i+rho do
-				x[#x][k-i+1] = torch.Tensor(song[k-i+1])
-			end
-			y[#y+1] = song[i+1]
-		end
-	end
-
-	return {x, y}
+	songs = normalize(songs, 92)
+	songs = normalize(songs, 93)
+	return songs
 end
