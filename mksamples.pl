4007242aabd19b30ff95358b4f557067b76eb00a
d <dennis.magnusson98@gmail.com>
2017-02-05 18:43:32 +0100

    Update mksamples

--- mksamples.pl
+++ mksamples.pl
@@ -1,13 +1,19 @@
 #!/usr/bin/perl
-
 use 5.010;
 
 $ARGV[0] or die("No model found");
 my $mname = substr $ARGV[0], 7, 99999; #Remove model/
+opendir my $dir, $ARGV[1] or die "No directory";
+my @files = readdir $dir;
+closedir $dir;
 
-system "mkdir generated/$mname";
-for(my $k = 1; $k < 5; $k += 0.5) {
-	for(my $temp = 0.5; $temp < 1.5; $temp += 0.25) {
-		system "th generator.lua -k $k -model $ARGV[0] -temperature $temp -o $mname/t"."$temp"."_k"."$k".".mid -len 1000";
+system "mkdir generated/$mname_$temp";
+
+for(my $temp = 1.0; $temp <= 1.5; $temp += 0.25) {
+	system "mkdir generated/$mname-$temp";
+	for(my $i = 0; $i < 10; $i++) {
+		my $f = $files[rand @files];
+		system "th generator.lua -model $ARGV[0] -temperature $temp -o $mname-$temp/$f.mid -len 500 -start $ARGV[1]/$f";#Generate file
+		system "timidity generated/$mname-$temp/$f.mid -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k generated/$mname-$temp/$i.mp3";#Convert to mp3
 	}
 }
