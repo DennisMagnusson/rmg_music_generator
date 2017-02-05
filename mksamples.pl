461d6624eaf2dd8ac45685c6e1688b913191e496
d <dennis.magnusson98@gmail.com>
2017-02-05 20:22:41 +0100

    Small value changes

--- mksamples.pl
+++ mksamples.pl
@@ -9,9 +9,9 @@ closedir $dir;
 
 system "mkdir generated/$mname_$temp";
 
-for(my $temp = 1.0; $temp <= 1.5; $temp += 0.25) {
+for(my $temp = 0.75; $temp <= 1.5; $temp += 0.25) {
 	system "mkdir generated/$mname-$temp";
-	for(my $i = 0; $i < 10; $i++) {
+	for(my $i = 0; $i < 4; $i++) {
 		my $f = $files[rand @files];
 		system "th generator.lua -model $ARGV[0] -temperature $temp -o $mname-$temp/$f.mid -len 500 -start $ARGV[1]/$f";#Generate file
 		system "timidity generated/$mname-$temp/$f.mid -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k generated/$mname-$temp/$i.mp3";#Convert to mp3
